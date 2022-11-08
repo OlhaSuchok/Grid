0fafe356679ad4f77ddb1fa9159fb251f39bc792
Olha-Suchok <suchok_olya@ukr.net>
2022-11-08 10:06:59 +0200

    Added

--- css/js/script.js
+++ css/js/script.js
@@ -139,10 +139,231 @@
 // } else {
 //     console.log(`Не партнер, скидка 0%`);
 // }
-
 // payment = payment - payment * discount;
 
 // console.log(`Оформлюємо замовлення на суму ${payment} зі знижкою ${discount * 100}%`);
 
 // totalSpent += payment;
 // console.log(`Загальна сума витраченого у магазині ${totalSpent});
+
+// Порахувати загальну суму покупок у магазині
+// const cart = [54, 28, 105, 70, 92];
+// let total = 0;
+
+// for (const value of cart) {
+//     total += value;
+// }
+// console.log('Total:' total)
+
+// Функція до прикладу вище
+// const calculateTotalPrice = function (items) {
+// let total = 0;
+
+// for (const value of items) {
+//     total += value;
+// }
+// return total;
+// }
+// const r1 = calculateTotalPrice([1, 2, 4, 6, 3, 33, 3]);
+
+// Додати таск (збільшимо елемент масиву)
+// Якщо елемент масиву  змінюється - використовуємо цикл з лічильником, а якщо не змінбється - цикл for of
+// const cart = [54, 28, 105, 70, 92];
+// let total = 0;
+
+// for (let i = 0; i < cart.length; i += 1) {
+//   cart[i] = Math.round(cart[i] * 1.1);
+//   total += value;
+// }
+// console.log(cart);
+
+// Підраховує суму усіх парних чисел у масиві
+// const numbers = [54, 28, 105, 70, 92];
+// let total = 0;
+
+// for (const number of numbers) {
+//   if (number % 2 === 0) {
+//       console.log(Парне число!);
+//       total += number;
+//     }
+// }
+// console.log(total);
+
+// Знайти вірний логін
+// const logins = ['mangos', 'fangos', 'olangos', 'tarangos'];
+// const loginFind = 'fangos';
+// let message = 'Користувача ${loginFind} не знайдено!';
+
+// for (const login of logins) {
+//   if (login === loginFind) {
+//     message = 'Користувача ${loginFind} знайдено!';
+//     break;
+//     }
+// }
+// console.log(message);
+
+// Функція для коду вище
+// const logins = ['mangos', 'fangos', 'olangos', 'tarangos'];
+
+// const findLogin = function (allLogins, loginToFind) {
+// for (const login of allLogins) {
+//   if (login === loginFind) {
+//     return 'Користувача ${loginFind} знайдено!';
+//     }
+// }
+// return 'Користувача ${loginFind} не знайдено!';
+// }
+// console.log(loginFind(logins, 'mangos'));
+
+// Або тернаргий оператор
+// const findLogin = function (allLogins, loginToFind) {
+// return allLogins.includes(loginToFind)
+// ? `Користувача ${loginFind} знайдено!`
+// : `Користувача ${loginFind} не знайдено!`;
+
+// return message;
+// }
+// console.log(loginFind(logins, 'mangos'));
+
+// Пошук найменшого числа
+// const numbers = [51, 28, 105, 70, 92];
+// const smallestNumber = numbers[0];
+
+// for (const number of numbers) {
+//   if (number < smallestNumber) {
+// smallestNumber = number;
+//     }
+// }
+
+// console.log(smallestNumber);
+
+// Функція до коду вище
+// const findSmallestNumber = function (numbers) {
+// let smallestNumber = numbers[0];
+
+// for (const number of numbers) {
+//   if (number < smallestNumber) {
+// smallestNumber = number;
+//     }
+// }
+// return smallestNumber;
+
+// console.log(findSmallestNumber[51, 28, 105, 70, 92]));
+
+// З'єднати в один рядок
+// Aбо метод Join
+// const friends = ['Mango', 'Poly', 'Kiwi', 'Ajax'];
+// let string = '';
+
+// for (const friend of friends) {
+// string += friend + ',';
+// }
+
+// string = string.slice(0, string.length - 1);
+
+// console.log(string);
+
+// Змінює регістр кожного символа в рядку
+// const string = 'JavaScript';
+// let letters = string.split('');
+
+// for (const letter of letters) {
+//   if (letter === letter.toLowerCase()) {
+//     console.log('Ця буква у нижньому регістрі!');
+//     invertedString += letter.toUpperCase();
+//   } else {
+//     console.log('Ця буква у верхньому регістрі!');
+//     invertedString += letter.toLowerCase();
+//   }
+// }
+
+// Або тернаргий оператор
+// invertedString +=
+// letter === letter.toLowerCase()
+// ? letter.toUpperCase()
+// : letter.toLowerCase();
+
+// console.log(invertedString);
+
+// Функція до коду вище
+// const changeCase = function (string) {
+// let invertedString = '';
+
+// for (const letter of letters) {
+// const isInLowerCase = letter === letter.toLowerCase();
+
+// invertedString += letter === letter.toLowerCase() ? letter.toUpperCase() : letter.toLowerCase();
+// }
+
+// return invertedString;
+// }
+// console.log(changeCase('krvkmLDSk')));
+
+// Змінити рядок до нижнього регістру з тире між словами
+// const title = 'Я люблю Java Script';
+// const title1 = title.toLowerCase().split(' ').join('-');
+
+// Функція до коду вище
+// const slugify = function (string) {
+// return = string.toLowerCase().split(' ').join('-');
+// }
+
+// Функція, яка приймає будь-яку кількість аргументів та повертає їх суму
+// const add = function (...args) {
+//     let total = 0;
+
+//     for (consr arg of args) {
+//         total += arg;
+//     }
+//     return total;
+// }
+
+// Функція, яка видає унікальні елементи, які є і у масиві і у arg
+// const filterNumber = function (array, ...args) {
+//     const uniquElevents = [];
+
+//     for (const element of array) {
+// if (args.includes(element)) {
+// uniquElevents.push(element)
+// }
+//     }
+// }
+//     return uniquElevents;
+// }
+
+// Рахує суму елементів двох масивів
+// const array1 = [5, 10, 15, 20, 77];
+// const array2 = [7, 14, 44, 57];
+// let total = 0;
+
+// const numbers = array1.concat(array2);
+
+// for (const number of numbers) {
+// total += number;
+// }
+
+// console.log(total);
+
+// Видалення елементу з масиву
+// const cards = ['Карточка 1', 'Карточка 2', 'Карточка 3', 'Карточка 4', 'Карточка 5'];
+
+// const cardToRemove = 'Карточка 1';
+// const index = cards.indexOf(cardToRemove);
+
+// cards.splice(index, 1);
+
+// Додаємо елемент по індекс
+// const cardToInsert = 'Карточка 6';
+// const index = cards.indexOf(cardToInsert);
+
+// cards.splice(index, 0, cardToInsert);
+
+// Деструктивні методи (оновлюють те, на чому викликаються): push, pop, splice
+
+// Функція для перебору масиву
+// const logItems = function (items) {
+// for (const item of items) {
+// }
+// }
+
+// logItems = [7, 14, 44, 57];
